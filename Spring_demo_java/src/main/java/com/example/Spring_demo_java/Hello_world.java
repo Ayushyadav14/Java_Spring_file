57fe86661016f6336fd2776fa2cdd17ffa61913f
Ayushyadav14 <ayushyadav14012002@gmail.com>
2026-06-03 09:21:30 +0530

    Added Spring Boot project setup with intellij

--- Spring_demo_java/src/main/java/com/example/Spring_demo_java/Hello_world.java
+++ Spring_demo_java/src/main/java/com/example/Spring_demo_java/Hello_world.java
@@ -1,4 +1,12 @@
 package com.example.Spring_demo_java;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Hello_world {
+    @GetMapping
+    public String hello(){
+        return  "Hello world from ayush";
+    }
 }
---
+++ intenralWorkingOfSpringBoot/src/main/java/com/example/intenralWorkingOfSpringBoot/IntenralWorkingOfSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.intenralWorkingOfSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntenralWorkingOfSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntenralWorkingOfSpringBootApplication.class, args);
+	}
+
+}
