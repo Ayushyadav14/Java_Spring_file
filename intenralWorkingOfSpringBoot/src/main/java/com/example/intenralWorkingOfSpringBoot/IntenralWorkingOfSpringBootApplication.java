ae9a3fb086dac8dcc7eac8b4469e3bb564d77ecf
Ayushyadav14 <ayushyadav14012002@gmail.com>
2026-06-03 11:25:11 +0530

    Internal Working of Spring Framework

--- intenralWorkingOfSpringBoot/src/main/java/com/example/intenralWorkingOfSpringBoot/IntenralWorkingOfSpringBootApplication.java
+++ intenralWorkingOfSpringBoot/src/main/java/com/example/intenralWorkingOfSpringBoot/IntenralWorkingOfSpringBootApplication.java
@@ -1,13 +1,27 @@
 package com.example.intenralWorkingOfSpringBoot;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IntenralWorkingOfSpringBootApplication {
+public class IntenralWorkingOfSpringBootApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IntenralWorkingOfSpringBootApplication.class, args);
+	}
+	//@Autowired
+	private PaymentService paymentService ;
+
+	public IntenralWorkingOfSpringBootApplication(PaymentService paymentService) {
+		this.paymentService = paymentService;
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		String payment = paymentService.pay();
+		System.out.println("Payment done: "+payment);
 	}
 
 }
---
+++ intenralWorkingOfSpringBoot/src/main/java/com/example/intenralWorkingOfSpringBoot/PaymentService.java
@@ -0,0 +1,5 @@
+package com.example.intenralWorkingOfSpringBoot;
+
+public interface PaymentService {
+    String pay();
+}
